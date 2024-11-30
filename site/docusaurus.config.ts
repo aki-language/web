5b132f89a8b30fa10dead7113a8d3443e5bfc385
Vincent Hengel <prelink835@gmail.com>
2024-11-30 23:29:32 +0100

    Update docusaurus.config.ts

--- site/docusaurus.config.ts
+++ site/docusaurus.config.ts
@@ -10,7 +10,7 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://aki.vhtek.net',
+  url: 'https://aki-lang.org',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
